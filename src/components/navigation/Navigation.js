734c6525d82b9570edb5ab98eb458d1bf55f9e27
cycycy <galoncyryll@gmail.com>
2019-01-01 22:25:22 -0800

    added github link in navbar

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,14 +5,14 @@ const Navigation = ({onRouteChange, isSignedIn }) => {
     if (isSignedIn) {
         return (
             <nav style={{display: 'flex'}}>
-                <a href="https://github.com/galoncyryll/face-recognition-app"style={{marginTop: '35px', marginRight: 'auto', width: '40px', height: '40px'}}><img alt='Logo' src={githubLogo} /></a>
+                <a href="https://github.com/galoncyryll/face-recognition-app"style={{marginTop: '35px', marginRight: 'auto', paddingLeft:'2rem'}} ><img style={{width: '40px', height: '40px'}}alt='Logo' src={githubLogo} /></a>
                 <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
             </nav>
         );
     } else {
         return (
             <nav style={{display: 'flex'}}>
-                <a href="https://github.com/galoncyryll/face-recognition-app"style={{marginTop: '35px', marginRight: 'auto', width: '40px', height: '40px'}} ><img alt='Logo' src={githubLogo} /></a>
+                <a href="https://github.com/galoncyryll/face-recognition-app"style={{marginTop: '35px', marginRight: 'auto', paddingLeft:'2rem'}} ><img style={{width: '40px', height: '40px'}}alt='Logo' src={githubLogo} /></a>
                 <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
                 <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
             </nav>
