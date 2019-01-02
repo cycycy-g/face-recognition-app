4f48d32e05caab793a47a780fa0f6cbdd4db8ab1
cycycy <galoncyryll@gmail.com>
2019-01-01 22:09:52 -0800

    added github link

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import githubLogo from './github.png';
 
 const Navigation = ({onRouteChange, isSignedIn }) => {
     if (isSignedIn) {
         return (
-            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <nav style={{display: 'flex'}}>
+                <a href="https://github.com/galoncyryll/face-recognition-app"style={{marginTop: '35px', marginRight: 'auto', width: '40px', height: '40px'}}><img alt='Logo' src={githubLogo} /></a>
                 <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
             </nav>
         );
     } else {
         return (
-            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <nav style={{display: 'flex'}}>
+                <a href="https://github.com/galoncyryll/face-recognition-app"style={{marginTop: '35px', marginRight: 'auto', width: '40px', height: '40px'}} ><img alt='Logo' src={githubLogo} /></a>
                 <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
                 <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
             </nav>
